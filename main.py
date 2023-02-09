28ee819115d73f4922f1400d69af47525a52a60b
Eron Cemal <eron.cemal@faculty.ai>
2023-02-09 11:13:24 +0000

    change man to dude

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def greeter(user_name: str) -> None:
     greeting_string = f"Hello {user_name}!"
     print(greeting_string)
-    print("How are you, man!?")
+    print("How are you, dude!?")
     print("How was your day?")
     print("How was your month?")
-    print("How was your year?!")
 
 
 
