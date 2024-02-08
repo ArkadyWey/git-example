cd0f181b6b0eae1477366dbf768fbaec3119f472
Eron Cemal <eron.cemal@faculty.ai>
2024-02-08 09:42:34 +0000

    add greeting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def greeter(user_name: str) -> None:
     greeting_string = f"Hello {user_name}!"
     print(greeting_string)
-
+    print("Hello from main branch -- create a conflicy Yall")
 
 
 if __name__ == "__main__":
